30ed526a901dd912686ba0cef0df8c65c1d37c1e
Kyle Buzsaki <kbuzsaki@ucsd.edu>
2016-09-16 18:22:26 -0700

    Add tests for blackout and short modes

--- rebuild_tests.sh
+++ rebuild_tests.sh
@@ -2,8 +2,11 @@
 
 TEST_OUTPUT_DIR="tests"
 
-for test_file in $(ls $TEST_OUTPUT_DIR); do
-    seed=$(echo "$test_file" | cut -d'.' -f 1)
-    echo "rebuilding $test_file"
-    ./generator $seed > $TEST_OUTPUT_DIR/$test_file
+for mode_dir in $(ls $TEST_OUTPUT_DIR); do
+    mode=$mode_dir
+    for test_file in $(ls $TEST_OUTPUT_DIR/$mode_dir); do
+        seed=$(echo "$test_file" | cut -d'.' -f 1)
+        echo "rebuilding $mode_dir/$test_file"
+        ./generator $seed $mode > $TEST_OUTPUT_DIR/$mode_dir/$test_file
+    done
 done
--- tester.sh
+++ tester.sh
@@ -3,23 +3,30 @@
 TEST_OUTPUT_DIR="tests"
 TEMP_FILE=".testoutput"
 
-passed=0
-failed=0
-for test_file in $(ls $TEST_OUTPUT_DIR); do
-    seed=$(echo "$test_file" | cut -d'.' -f 1)
-    ./generator $seed > $TEMP_FILE
+for mode_dir in $(ls $TEST_OUTPUT_DIR); do
+    mode=$mode_dir
+    echo "testing mode '$mode'"
 
-    cmp $TEMP_FILE $TEST_OUTPUT_DIR/$test_file > /dev/null
-    return_code="$?"
-    if [ "$return_code" == "0" ]; then
-        passed=$((passed + 1))
-        echo "$test_file passed!"
-    else
-        failed=$((failed + 1))
-        echo "$test_file Failed :("
-    fi
+    passed=0
+    failed=0
+    for test_file in $(ls $TEST_OUTPUT_DIR/$mode_dir); do
+        seed=$(echo "$test_file" | cut -d'.' -f 1)
+        ./generator $seed $mode > $TEMP_FILE
+
+        cmp $TEMP_FILE $TEST_OUTPUT_DIR/$mode_dir/$test_file > /dev/null
+        return_code="$?"
+        if [ "$return_code" == "0" ]; then
+            passed=$((passed + 1))
+            echo "$test_file passed!"
+        else
+            failed=$((failed + 1))
+            echo "$test_file Failed :("
+        fi
+    done
+
+    echo ""
+    echo "$passed passed"
+    echo "$failed failed"
+    echo ""
 done
 
-echo ""
-echo "$passed passed"
-echo "$failed failed"
